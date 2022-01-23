eb5f1b8c362d061492cf324952cd93dd29af3d6c
Andrija Jelenkovic <andrija.jelenkovic@icentic.com>
2022-01-23 20:55:59 +0100

    Format code.

--- resources/views/userList.php
+++ resources/views/userList.php
@@ -1,36 +1,41 @@
 <?php
-/* @var $paginatedResponse PaginatedResponse*/
+/* @var $paginatedResponse PaginatedResponse */
 
 use Amdrija\RealEstate\Application\Models\User;
 use Amdrija\RealEstate\Application\RequestModels\PaginatedResponse;
 
 ?>
 
-<h2 class="uk-heading-small uk-margin-top">Users</h2>
-<table class="uk-table uk-table-hover uk-table-divider">
-    <thead>
-    <tr>
-        <th></th>
-        <th>Username</th>
-        <th>Email</th>
-        <th class="uk-text-center">Verified</th>
-        <th class="uk-text-center">Administrator</th>
-    </tr>
-    </thead>
-    <tbody>
-    <?php /* @var $user User*/?>
-    <?php foreach($paginatedResponse->data as $user):?>
-        <tr class="" ondblclick="window.location='/admin/users/edit/<?= $user->id?>'">
-            <td><input class="uk-checkbox" type="checkbox"></td>
-            <td><?= $user->userName?></td>
-            <td><?= $user->email?></td>
-            <td class="uk-text-center"><input class="uk-checkbox" type="checkbox" <?= $user->verified ? "checked" : ""?> disabled></td>
-            <td class="uk-text-center"><input class="uk-checkbox" type="checkbox" <?= $user->isAdministrator ? "checked" : ""?> disabled></td>
+    <h2 class="uk-heading-small uk-margin-top">Users</h2>
+    <table class="uk-table uk-table-hover uk-table-divider">
+        <thead>
+        <tr>
+            <th></th>
+            <th>Username</th>
+            <th>Email</th>
+            <th class="uk-text-center">Verified</th>
+            <th class="uk-text-center">Administrator</th>
         </tr>
-    <?php endforeach; ?>
-    </tbody>
-</table>
-    <a href="/admin/users/add"><div class="uk-card uk-card-hover uk-text-center uk-padding-small uk-text-large">+</div></a>
+        </thead>
+        <tbody>
+        <?php /* @var $user User */ ?>
+        <?php foreach ($paginatedResponse->data as $user): ?>
+            <tr class="" ondblclick="window.location='/admin/users/edit/<?= $user->id ?>'">
+                <td><input class="uk-checkbox" type="checkbox"></td>
+                <td><?= $user->userName ?></td>
+                <td><?= $user->email ?></td>
+                <td class="uk-text-center"><input class="uk-checkbox"
+                                                  type="checkbox" <?= $user->verified ? "checked" : "" ?> disabled></td>
+                <td class="uk-text-center"><input class="uk-checkbox"
+                                                  type="checkbox" <?= $user->isAdministrator ? "checked" : "" ?>
+                                                  disabled></td>
+            </tr>
+        <?php endforeach; ?>
+        </tbody>
+    </table>
+    <a href="/admin/users/add">
+        <div class="uk-card uk-card-hover uk-text-center uk-padding-small uk-text-large">+</div>
+    </a>
 
 
 <?php include __DIR__ . '/../templates/pagination.php' ?>
